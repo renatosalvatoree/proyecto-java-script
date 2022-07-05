2602b6f386752d110e2d1991c7be82a877ae450e
Renato Salvatore <renatopolo13@icloud.com>
2022-07-05 18:18:58 -0300

    Implementando buscador con boton de submit.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,38 @@
+ const mangas = [{
+         nombre: "hunter x hunter",
+         id: "hunterXHunter"
+     },
+     {
+         nombre: "jojo - steel ball run",
+         id: "steelBallRun"
+     },
+     {
+         nombre: "chainsaw man",
+         id: "chainsawMan"
+     },
+     {
+         nombre: "fairy tail",
+         id: "fairyTail"
+     },
+     {
+         nombre: "spy x family",
+         id: "spyXFamily"
+     },
+     {
+         nombre: "jojo - stone ocean",
+         id: "stoneOcean"
+     }
+ ]
 
-
-function realizarBusqueda(){
-    const busqueda = document.getElementById("busqueda").value;
-    alert(busqueda);
-    const busquedaPorNombre = mangas.filter((n) => n.nombre.includes(busqueda));
-    console.log("Encotramos las siguientes opciones:");
-    for (const resultado of busquedaPorNombre) {
-        console.log("El manga " + resultado.nombre + " de " + resultado.autor + " que cuesta " + resultado.precio + " euros.");
+ function realizarBusqueda() {
+    for (const todosLosMangas of mangas){
+        const reset = document.getElementById(todosLosMangas.id);
+        reset.className = "didntSearchFor"
     }
-    
-    console.log(Object.nombre);
+     const busqueda = document.getElementById("busqueda").value;
+     const resultadoBusqueda = mangas.filter((n) => n.nombre.includes(busqueda));
+     for (const resultado of resultadoBusqueda) {
+        let encontrados = document.getElementById(resultado.id);
+        encontrados.className = "searchResult"
 }
-
-const mangas = [
-    {nombre: "Hunter x Hunter", autor: "Yoshihiro Togashi", precio: 7},
-    {nombre: "Steel Ball Run", autor: "Hirohiko Araki", precio: 15},
-    {nombre: "Chainsaw Man", autor: "Tatsuki Fujimoto", precio: 12}
-]
-
-
-console.log(busquedaPorNombre.nombre)
-
-
-    
+     }
