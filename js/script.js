16052e7b40abc53a4c4ee89cd5eafc13dbc6ab47
Renato Salvatore <renatopolo13@icloud.com>
2022-07-06 20:37:32 -0300

    Continuando con el disenio general del sitio.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
- const mangas = [{
-         nombre: "hunter x hunter",
-         id: "hunterXHunter"
-     },
-     {
-         nombre: "jojo - steel ball run",
-         id: "steelBallRun"
-     },
-     {
-         nombre: "chainsaw man",
-         id: "chainsawMan"
-     },
-     {
-         nombre: "fairy tail",
-         id: "fairyTail"
-     },
-     {
-         nombre: "spy x family",
-         id: "spyXFamily"
-     },
-     {
-         nombre: "jojo - stone ocean",
-         id: "stoneOcean"
-     }
- ]
+const mangas = [{
+        nombre: "hunter x hunter",
+        id: "hunterXHunter"
+    },
+    {
+        nombre: "jojo - steel ball run",
+        id: "steelBallRun"
+    },
+    {
+        nombre: "chainsaw man",
+        id: "chainsawMan"
+    },
+    {
+        nombre: "fairy tail",
+        id: "fairyTail"
+    },
+    {
+        nombre: "spy x family",
+        id: "spyXFamily"
+    },
+    {
+        nombre: "jojo - stone ocean",
+        id: "stoneOcean"
+    }
+]
 
- function realizarBusqueda() {
+let busqueda = document.getElementById("submit")
+busqueda.onclick = realizarBusqueda();
+
+function realizarBusqueda() {
     for (const todosLosMangas of mangas){
         const reset = document.getElementById(todosLosMangas.id);
         reset.className = "didntSearchFor"
     }
-     const busqueda = document.getElementById("busqueda").value;
-     const resultadoBusqueda = mangas.filter((n) => n.nombre.includes(busqueda));
-     for (const resultado of resultadoBusqueda) {
+    const busqueda = document.getElementById("busqueda").value;
+    const resultadoBusqueda = mangas.filter((n) => n.nombre.includes(busqueda));
+    for (const resultado of resultadoBusqueda) {
         let encontrados = document.getElementById(resultado.id);
         encontrados.className = "searchResult"
-}
-     }
+    }}
